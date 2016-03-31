203c8154eb03172d9c53d63518f9a437d11bdb9e
Maureen Haley <haleyma@earthlink.net>
2016-03-31 15:46:35 -0400

    add bootstrap files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Random Quote Javascript//
+
+   /*var rumiQuotes = "['“Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.”  ― Rumi',
+                          '“Out beyond ideas of wrongdoing and rightdoing there is a field. I'll meet you there./nWhen the soul lies down in that grass the world is too full to talk about.”  ― Rumi',
+					'“The wound is the place where the Light enters you.”  ― Rumi',                          
+					'“Stop acting so small. You are the universe in ecstatic motion.”  ― Rumi',
+                          '“What you seek is seeking you.”  ― Rumi',
+                          '“Don't be satisfied with stories, how things have gone with others. Unfold your own myth.”  ― Rumi'];
+
+
+     function newQuote(){
+     	document.getElementById("rumiQuote").innerHTML = "New Quote";
+     }*/
